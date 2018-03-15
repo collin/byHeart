fd3b3f9866c299145aaa6b18ee7e536260637e01
Vaida Armanaviciute <vaidaarman@gmail.com>
2018-03-15 16:44:44 -0500

    single quotes fixed

--- client/components/NewPassage.js
+++ client/components/NewPassage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Input, TextArea, Button, Label, Segment, Header } from 'semantic-ui-react'
+import { Form, Input, TextArea, Button, Label, Segment } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { gotPassage } from '../store'
 
@@ -18,13 +18,13 @@ class NewPassage extends Component {
     return (
 
 
-      <Segment style={{ marginLeft: "2%", marginRight: "2%" }}>
+      <Segment style={{ marginLeft: '2%', marginRight: '2%' }}>
         <Form onSubmit={handleSubmit}>
           <Label pointing="below" size="large">Please enter the passage's title and content</Label>
-          <Input style={{ width: "100%", marginLeft: "0" }} id="formTitle" name="passageTitle" placeholder="Title" />
-          <TextArea id="formContent" autoHeight style={{ minHeight: 200 }} name="passageContent" label="Passage" placeholder="Passage" />          
-          <div style={{width: "100%"}}>
-            <Button type="submit" content="Start" floated="right" style={{ marginRight: "2%" }} />
+          <Input style={{ width: '100%', marginLeft: "0" }} id="formTitle" name="passageTitle" placeholder="Title" />
+          <TextArea id="formContent" autoHeight style={{ minHeight: 200 }} name="passageContent" label="Passage" placeholder="Passage" />
+          <div style={{ width: '100%' }}>
+            <Button type="submit" content="Start" floated="right" style={{ marginRight: '2%' }} />
           </div>
         </Form>
       </Segment>
