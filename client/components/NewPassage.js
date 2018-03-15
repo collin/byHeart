0370eb53d6121329cb494b6720c38cecbefd5dd2
Vaida Armanaviciute <vaidaarman@gmail.com>
2018-03-15 13:22:55 -0500

    local passage persists. will work on styling

--- client/components/NewPassage.js
+++ client/components/NewPassage.js
@@ -18,8 +18,8 @@ class NewPassage extends Component {
     return (
       <div id="form">
         <p>Enter your passage title and text in the fields bello and click START:</p>
-        <Form size="large" onSubmit={handleSubmit}>
-          <Form.Field id="formTitle" name="passageTitle" control={Input} label="Title" placeholder="Enter a title for your passage" />
+        <Form fluid="true" onSubmit={handleSubmit}>
+          <Form.Field id="formTitle"  name="passageTitle" control={Input} label="Title" placeholder="Enter a title for your passage" />
           <Form.Field id="formContent" name="passageContent" control={TextArea} label="Passage" placeholder="Enter or paste your passage here..." />
           <div>
             <Form.Button control={Button} content="Start" />
