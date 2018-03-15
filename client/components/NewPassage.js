ef76af47d01b2639cc058e9f7f2dc0adb5165d97
Vaida Armanaviciute <vaidaarman@gmail.com>
2018-03-15 12:41:59 -0500

    persist local passage on refresh

--- client/components/NewPassage.js
+++ client/components/NewPassage.js
@@ -1,44 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, Input, TextArea, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { gotPassage } from '../store'
 
-const NewPassage = (props) => {
+class NewPassage extends Component {
 
-  // function componentDidMount() {
-  //   if (localStorage.passageTitle || localStorage.passageText) {
+  componentDidMount() {
+    if (!this.props.isLoggedIn && localStorage.passage) {
+      document.getElementById('formTitle').value = JSON.parse(localStorage.getItem('passage')).title
+      document.getElementById('formContent').value = JSON.parse(localStorage.getItem('passage')).content
+    }
+  }
+  render() {
 
-  //   }
-  // }
-  const { handleSubmit } = props
+    const { handleSubmit } = this.props
 
-  return (
-    <div id="form">
-      <p>Enter your passage title and text in the fields bello and click START:</p>
-      <Form size="large" onSubmit={handleSubmit}>
-        <Form.Field width={12} name="passageTitle" control={Input} label="Title" placeholder="Enter a title for your passage" />
-        <Form.Field width={12} name="passageContent" control={TextArea} label="Passage" placeholder="Enter or paste your passage here..." />
-        <div>
-          <Form.Button control={Button} content="Start" />
-        </div>
-      </Form>
-    </div>
-  )
+    return (
+      <div id="form">
+        <p>Enter your passage title and text in the fields bello and click START:</p>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Form.Field id="formTitle" name="passageTitle" control={Input} label="Title" placeholder="Enter a title for your passage" />
+          <Form.Field id="formContent" name="passageContent" control={TextArea} label="Passage" placeholder="Enter or paste your passage here..." />
+          <div>
+            <Form.Button control={Button} content="Start" />
+          </div>
+        </Form>
+      </div>
+    )
+  }
 }
 
 function setPassageLocals(passage) {
   localStorage.setItem('passage', JSON.stringify(passage))
 }
 
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.user.id
+  }
+}
+
 const mapDispatchToProps = (dispatch) => {
   return {
     handleSubmit(event) {
       event.preventDefault()
-      const passage = {title: event.target.passageTitle.value, content: event.target.passageContent.value}
+      const passage = { title: event.target.passageTitle.value, content: event.target.passageContent.value }
       setPassageLocals(passage)
       dispatch(gotPassage(passage))
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(NewPassage)
+export default connect(mapState, mapDispatchToProps)(NewPassage)
