848c0389e2107ffd369688dfb656d9e0d0c359b9
Kevin Kelly <kvinklly@gmail.com>
2018-03-18 09:45:26 -0500

    update passage spec added

--- client/store/passages.spec.js
+++ client/store/passages.spec.js
@@ -1,5 +1,5 @@
 // import reducer, { gotPassages, fetchPassages } from './passages'
-import reducer, { gotPassages, fetchPassages } from './passages'
+import reducer, { gotPassages, fetchPassages, updatePassage } from './passages'
 import { expect } from 'chai'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
@@ -48,6 +48,20 @@ describe('Passages store', () => {
             const actions = store.getActions()
             expect(actions[0].type).to.be.equal('GOT_PASSAGES')
             expect(actions[0].passages).to.deep.equal(fakePassages)
+          })
+      })
+    })
+
+    describe('updatePassage', () => {
+      it('dispatches the GOT_NEW_PASSAGE action', () => {
+        const fakePassage = { id: 1, title: 'My title', content: 'some lines to learn', isPublic: true, authorId: 1 }
+        mockAxios.onPut('/api/passages/1').replyOnce(204, fakePassage)
+
+      return store.dispatch(updatePassage({ id: 1, title: 'My title', content: 'some lines to learn', isPublic: true, authorId: 1 }))
+          .then(() => {
+            const actions = store.getActions()
+            expect(actions[0].type).to.be.equal('GOT_NEW_PASSAGE')
+            expect(actions[0].passage.title).to.equal('My title')
           })
       })
     })
