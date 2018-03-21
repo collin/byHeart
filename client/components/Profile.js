59f7779ed0d80e4f8bd1d9c4bb49f843548ee7e1
Vaida Armanaviciute <vaidaarman@gmail.com>
2018-03-21 13:39:42 -0500

    profile cards styled

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { gotPassage, fetchPassages } from '../store/'
-import { Card, Button } from 'semantic-ui-react'
+import { Card, Button, Icon, Label, Popup, Container, Segment } from 'semantic-ui-react'
 import history from '../history'
 import './Profile.css'
 
@@ -15,27 +15,71 @@ export class Profile extends Component {
   render() {
     const { passages, user, handleNewPassage, handleTrainPassage, handleEditPassage } = this.props
     const filteredPassages = passages.filter(passage => passage.authorId === user.id)
-
+    filteredPassages && console.log(filteredPassages)
     return (
       <div className="main">
         <h1>My Profile</h1>
-        <Card.Group className="passage-cards" itemsPerRow={2}>
-          {filteredPassages && filteredPassages.map(passage => {
-            return (
-              <Card key={`passage-${passage.id}`} className="card" basic color="purple">
-                <Card.Content header={passage.title} />
-                <Card.Content description={passage.content.slice(0, 40).concat('(...)')} />
-                <Card.Content extra>
-                  <Button onClick={() => { handleTrainPassage(passage) }}>Train</Button>
-                  <Button onClick={() => { handleEditPassage(passage) }}>Edit</Button>
-                  <Button>Delete</Button>
-                </Card.Content>
-              </Card>
-            )
-          })}
-        </Card.Group>
-        <div>
-          <Button onClick={handleNewPassage}>New Passage</Button>
+        <div className="passage-cards">
+          <Card.Group itemsPerRow={3}>
+            <Card>
+              <Card.Content id="newCard" textAlign="center" color="grey">
+                <Button animated="fade" size="huge" className="cardButton" onClick={handleNewPassage}>
+                  <Button.Content hidden>Add New</Button.Content>
+                  <Button.Content visible>
+                    <Icon name="plus square outline" size="huge" />
+                  </Button.Content>
+                </Button>
+              </Card.Content>
+            </Card>
+            {filteredPassages && filteredPassages.map(passage => {
+              return (
+                <Card key={`passage-${passage.id}`} className="card" color="purple" centered>
+                  <Card.Content>
+                    <Button floated="right" animated="vertical" size="mini" className="cardButton">
+                      <Button.Content hidden>Delete</Button.Content>
+                      <Button.Content visible>
+                        <Icon name="delete" size="small" />
+                      </Button.Content>
+                    </Button>
+                    <Card.Header style={{overflowWrap: 'break-word'}}>
+                        {passage.title}
+                    </Card.Header>
+                    <Card.Description style={{overflowWrap: 'break-word'}}>
+                      {passage.content.slice(0, 40).concat('(...)')}
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Button animated="vertical" size="mini" className="cardButton" onClick={() => { handleTrainPassage(passage) }}>
+                      <Button.Content hidden>Train</Button.Content>
+                      <Button.Content visible>
+                        <Icon name="file text outline" size="large" />
+                      </Button.Content>
+                    </Button>
+                    <Button animated="vertical" size="mini" className="cardButton" onClick={() => { handleEditPassage(passage) }}>
+                      <Button.Content hidden>Edit</Button.Content>
+                      <Button.Content visible>
+                        <Icon name="edit" size="large" />
+                      </Button.Content>
+                    </Button>
+                    <Popup
+                      trigger={
+                        <Button as="div" size="mini" floated="right" labelPosition="right">
+                          <Button color="purple">
+                            <Icon name="clock" size="large" />
+                          </Button>
+                          <Label as="a" basic color="purple" pointing="left">00:00:08:4</Label>
+                        </Button>
+                      }
+                      content="Last time you read this passage in this amount of time"
+                      position="bottom center"
+                      on="hover"
+                      inverted
+                    />
+                  </Card.Content>
+                </Card>
+              )
+            })}
+          </Card.Group>
         </div>
       </div>
     )
